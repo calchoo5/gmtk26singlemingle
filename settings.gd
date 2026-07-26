0e81f888f4134af5f6d1abf54a9388cb6777fb06
Calvin Choo <calchoo2@gmail.com>
2026-07-26 02:32:22 -0400

    added bgm and stuff
    yeah

--- settings.gd
+++ settings.gd
@@ -3,12 +3,19 @@ extends Control
 var mode := DisplayServer.window_get_mode()
 var is_window: bool = mode != DisplayServer.WINDOW_MODE_FULLSCREEN
 
+var master = AudioServer.get_bus_index("Master")
+var bgm = AudioServer.get_bus_index("BGM")
+var sfx = AudioServer.get_bus_index("SFX")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if is_window:
 		%fullscreen.button_pressed = false
 	else:
 		%fullscreen.button_pressed = true
+	%master.value = AudioServer.get_bus_volume_linear(master)
+	%bgm.value = AudioServer.get_bus_volume_linear(bgm)
+	%sfx.value = AudioServer.get_bus_volume_linear(sfx)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,15 +24,15 @@ func _process(delta: float) -> void:
 
 
 func _on_master_value_changed(value: float) -> void:
-	pass # Replace with function body.
+	AudioServer.set_bus_volume_db(master,linear_to_db(value))
 
 
 func _on_bgm_value_changed(value: float) -> void:
-	pass # Replace with function body.
+	AudioServer.set_bus_volume_db(bgm,linear_to_db(value))
 
 
 func _on_sfx_value_changed(value: float) -> void:
-	pass # Replace with function body.
+	AudioServer.set_bus_volume_db(sfx,linear_to_db(value))
 
 
 func _on_fullscreen_toggled(toggled_on: bool) -> void:
