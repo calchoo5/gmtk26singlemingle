ef1846534eddf4f403f87402e0854cee1c8080af
Calvin Choo <calchoo2@gmail.com>
2026-07-26 07:32:30 -0400

    added the daters
    jdtghfdshfshfg

--- main.gd
+++ main.gd
@@ -6,6 +6,30 @@ extends Control
 @onready var cafe = load("res://assets/images/Cafeteria_Day.png")
 @onready var cafebgm = load("res://assets/audio/atlasaudio-energetic-energetic-music-507828.mp3")
 
+
+
+@onready var suitors = {
+	"irish": load("res://assets/images/ppl/12022868-isolated-4570748_1920.png"),
+	"martial": load("res://assets/images/ppl/alvorcem-arnis-kali-3342113_1920.png"),
+	"anon": load("res://assets/images/ppl/geierweb-guy-4628808_1920.png"),
+	'horse': load("res://assets/images/ppl/maja7777-horse-head-3730326_1920.png"),
+	"wbook": load("res://assets/images/ppl/maja7777-woman-5309387_1920.png"),
+	'jester': load("res://assets/images/ppl/momentmal-jester-2835285_1920.png"),
+	'posed': load("res://assets/images/ppl/nati0988-boy-1080167_1920.png"),
+	'mbook': load("res://assets/images/ppl/omerzarnaab-student-3496312_1920.png"),
+	'omisido': load("res://assets/images/ppl/omisido-smile-3660166_1920.png"),
+	'thumbs': load("res://assets/images/ppl/publicdomainpng-guy-3220439_1920.png"),
+	'stare': load("res://assets/images/ppl/publicdomainpng-guy-3237859_1920.png"),
+	'girl': load("res://assets/images/ppl/publicdomainpng-model-3296470_1920.png"),
+	'suit': load("res://assets/images/ppl/publicdomainpng-photo-shoot-3321569_1920.png"),
+	'asian': load("res://assets/images/ppl/rauschenberger-person-3248202_1920.png"),
+	'camera': load("res://assets/images/ppl/thehilaryclark-isolated-1182220_1920.png"),
+	'doc': load("res://assets/images/ppl/thehilaryclark-isolated-1188036_1920.png"),
+	'thinker': load("res://assets/images/ppl/thehilaryclark-isolated-1197345_1920.png"),
+	'sad': load("res://assets/images/ppl/thehilaryclark-man-2285605_1920.png"),
+}
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# 1. Clear any broken editor-canned data safely
@@ -31,9 +55,13 @@ func _on_setbutton_pressed() -> void:
 func _on_date_pressed() -> void:
 	$BG.texture = cafe
 	$starting.visible = false
-	Global.firstplay = false
 	Bgm.stream = cafebgm
 	Bgm.play()
+	if Global.firstplay:
+		dgb.start('datetest')
+		Global.firstplay = false
+	else:
+		dgb.start('date')
 	
 
 
@@ -45,7 +73,10 @@ func _on_store_pressed() -> void:
 func _on_dialogue_box_dialogue_signal(value: String) -> void:
 	if value == "finito":
 		$starting.visible = true
+	if value == "datego":
+		$dater.texture = suitors[suitors.keys()[randi()%suitors.size()]]
 
 
 func _on_quitting_pressed() -> void:
-	get_tree().change_scene_to_file("res://menu.tscn")
+	$dater.texture = suitors[suitors.keys()[randi()%suitors.size()]]
+	#get_tree().change_scene_to_file("res://menu.tscn")
---
+++ timer.gd
@@ -0,0 +1,20 @@
+extends Label
+
+var time_left: float = 20.0 # Start time in seconds
+var is_running: bool = true
+
+func _process(delta: float) -> void:
+	if is_running and time_left > 0.0:
+		time_left -= delta
+		if time_left <= 0.0:
+			time_left = 0.0
+			is_running = false
+			text = "00:00"
+		else:
+			update_display()
+
+func update_display() -> void:
+	var total_seconds: int = int(time_left)
+	var milliseconds: int = int((time_left - total_seconds) * 100)
+	
+	text = "%02d:%02d" % [total_seconds, milliseconds]
